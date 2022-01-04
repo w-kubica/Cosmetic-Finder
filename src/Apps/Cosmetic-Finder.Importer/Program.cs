537aed14bed194dd37397a741be38b817a41f4a3
w-kubica <w.kubicaweronika@gmail.com>
2022-01-04 22:52:34 +0100

    Init project

---
+++ src/Apps/Cosmetic-Finder.Importer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cosmetic_Finder.Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
