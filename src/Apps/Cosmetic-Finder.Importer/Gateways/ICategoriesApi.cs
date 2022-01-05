46af24fef8fe0855f9b8ad8e8cf87517d1e51474
w-kubica <w.kubicaweronika@gmail.com>
2022-01-05 15:19:58 +0100

    Refactor

---
+++ src/Apps/Cosmetic-Finder.Importer/Gateways/ICategoriesApi.cs
@@ -0,0 +1,12 @@
+﻿using Cosmetic_Finder.Importer.Response;
+using Refit;
+using System.Threading.Tasks;
+
+namespace Cosmetic_Finder.Importer.Gateways
+{
+    public interface ICategoriesApi
+    {
+        [Get("/Products?CategoryId={CategoryId}&PageSize=5")]
+        Task<ResponseCategory> Get(int CategoryId);
+    }
+}
---
+++ src/Apps/Cosmetic-Finder.Importer/Response/ResponseCategory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Cosmetic_Finder.Importer.Response
+{
+    public class ResponseCategory
+    {
+        public Data Data { get; set; }
+
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string NavigateUrl { get; set; }
+        // public string brand { get; set; }
+        // public string caption { get; set; }
+        // public string Category { get; set; }
+    }
+
+    public class Data
+    {
+        public List<Product> products { get; set; }
+        public int totalCount { get; set; }
+    }
+}
