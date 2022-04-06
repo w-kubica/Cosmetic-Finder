caa06eda6b9dc7d76a07a4039ff6e2e79afc9a42
Weronika Kubica <w.kubicaweronika@gmail.com>
2022-04-06 22:50:40 +0200

    Add TODO

--- src/Cosmetic-Finder.Infrastructure/Gateways/ApiConst.cs
+++ src/Cosmetic-Finder.Infrastructure/Gateways/ApiConst.cs
@@ -2,5 +2,6 @@ namespace Cosmetic_Finder.Infrastructure.Gateways;
 
 public static class ApiConst
 {
+    //todo: move to appsettings
     public const string RossmannPortalUrl = "https://www.rossmann.pl";
 }
