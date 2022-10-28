fec275b336f3262a104a951e3b422a494ef20a74
Weronika Kubica <w.kubicaweronika@gmail.com>
2022-10-28 23:37:15 +0200

    Clean up

--- src/Cosmetic-Finder.Infrastructure/Gateways/ApiConst.cs
+++ src/Cosmetic-Finder.Infrastructure/Gateways/ApiConst.cs
@@ -2,7 +2,5 @@ namespace Cosmetic_Finder.Infrastructure.Gateways;
 
 public static class ApiConst
 {
-    //todo: move to appsettings
-    //https://www.rossmann.pl/products/api/Products?CategoryId=8686&PageSize=6500
     public const string RossmannPortalUrl = "https://www.rossmann.pl";
 }
--- src/Cosmetic-Finder.Infrastructure/Gateways/IProductsApi.cs
+++ src/Cosmetic-Finder.Infrastructure/Gateways/IProductsApi.cs
@@ -5,7 +5,6 @@ namespace Cosmetic_Finder.Infrastructure.Gateways;
 
 public interface IProductsApi
 {
-    //https://www.rossmann.pl/products/api/Products?CategoryId=8686&PageSize=6500
     [Get("/Products?CategoryId={CategoryId}&Page={Page}")]
     Task<Products> Get(int categoryId, int page);
 }
